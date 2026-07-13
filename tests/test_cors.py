77e32bb40bc0807f145c11d667b31c7e8aaa0ce4
Fatih Emre Yüce <fatihemreyuce@gmail.com>
2026-07-13 11:47:13 +0300

    test: CORS yasakli origin regresyon testi ekle

--- tests/test_cors.py
+++ tests/test_cors.py
@@ -1,3 +1,8 @@
 def test_cors_allows_configured_origin(client):
     resp = client.get("/", headers={"Origin": "http://localhost:3000"})
     assert resp.headers.get("access-control-allow-origin") == "http://localhost:3000"
+
+
+def test_cors_rejects_unconfigured_origin(client):
+    resp = client.get("/", headers={"Origin": "http://evil.example.com"})
+    assert "access-control-allow-origin" not in resp.headers
